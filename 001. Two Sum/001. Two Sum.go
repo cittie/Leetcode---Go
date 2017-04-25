54349cc5f985f415416169515f22efe88bd4fc20
Yee <cittie.yi@gmail.com>
2017-04-25 21:02:45 +0800

    Modify to make it easier to read.

--- 001. Two Sum/001. Two Sum.go
+++ 001. Two Sum/001. Two Sum.go
@@ -4,12 +4,12 @@ func twoSum(nums []int, target int) []int {
 	numMap := map[int]int{}
 	result := make([]int, 2)
 
-	for i := range nums {
-		_, ok := numMap[nums[i]]
+	for i, num := range nums {
+		_, ok := numMap[num]
 		if ok {
-			result[0], result[1] = i, numMap[nums[i]]
+			result[0], result[1] = i, numMap[num]
 		} else {
-			numMap[target-nums[i]] = i
+			numMap[target - num] = i
 		}
 	}
 
