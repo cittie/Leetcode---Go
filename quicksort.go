a9857225a9a282a81459d6d92740c5ddbae1948f
JiangYi <jiangyi@taiyouxi.cn>
2021-01-29 16:20:08 +0800

    3 way quick sort

--- quicksort.go
+++ quicksort.go
@@ -1,6 +1,8 @@
 package leetcode
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 // qsort is a 2 way quick sort solution
 func qsort(slice []int) {
@@ -32,4 +34,42 @@ func qsort(slice []int) {
 	// Recursion
 	qsort(slice[:left])
 	qsort(slice[left+1:])
+}
+
+// qsort3 is a 3 way quick sort solution
+func qsort3(slice []int) {
+	n := len(slice)
+	if n < 2 {
+		return
+	}
+
+	swap := func(a, b int) {
+		slice[a], slice[b] = slice[b], slice[a]
+	}
+
+	// pick and move the pivot to the left
+	pivotIdx := rand.Intn(n)
+	pivotV := slice[pivotIdx]
+	swap(0, pivotIdx)
+
+	l, r := 0, n
+	for i := 1; i < r; i++ {
+		if slice[i] < pivotV {
+			// move elements smaller than pivot to the left
+			swap(i, l+1)
+			l++
+		} else if slice[i] > pivotV {
+			// move elements bigger than pivot to the right
+			swap(i, r-1)
+			r--
+			i--
+		}
+	}
+
+	// move the pivot(s) after the last smaller element
+	swap(0, l)
+
+	// Recursion
+	qsort3(slice[:l])
+	qsort3(slice[r:])
 }
--- quicksort_test.go
+++ quicksort_test.go
@@ -16,10 +16,39 @@ func TestQuicksort(t *testing.T) {
 		{[]int{5, 1}, []int{1, 5}},
 		{[]int{5, 5, 5}, []int{5, 5, 5}},
 		{[]int{2, 1, 14, 25, 7, 2444}, []int{1, 2, 7, 14, 25, 2444}},
+		{
+			[]int{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 5, 22, 211, 211, 2211, 22233, 33321, 2312, 132, 546, 77567, 7675, 767, 88, 88, 88, 88, 88, 88, 45646, 74564, 464564, 4564564, 648867},
+			[]int{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 5, 22, 88, 88, 88, 88, 88, 88, 132, 211, 211, 546, 767, 2211, 2312, 7675, 22233, 33321, 45646, 74564, 77567, 464564, 648867, 4564564},
+		},
 	}
 
 	for _, test := range tests {
 		qsort(test.in)
 		assert.Equal(t, test.out, test.in)
 	}
+
+	for _, test := range tests {
+		qsort3(test.in)
+		assert.Equal(t, test.out, test.in)
+	}
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	src := []int{
+		1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 5, 22, 211, 211, 2211, 22233, 33321, 2312, 132, 546, 77567, 7675, 767, 88, 88, 88, 88, 88, 88, 45646, 74564, 464564, 4564564, 648867,
+	}
+
+	b.Run("quick sort 2 way", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			qsort(src)
+		}
+	})
+
+	b.Run("quick sort 3 way", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			qsort3(src)
+		}
+	})
 }
